811155a08d3386207c4efeaa380cc98b8d493fe5
nawrotadam <nawrotadam@onet.pl>
2020-10-23 11:26:42 -0700

    Added simple randomising function, created empty result quality estimation function

---
+++ doc/notes.md
@@ -0,0 +1,6 @@
+dane wejsciowe:
+    ilosc nalesnikow = n (unsigned int)
+    
+
+dane wyjsciowe:
+    liczba przelozen potrzebna do uporzadkowania stosu = k (unsigned int)
--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <cstdlib>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+// randomise number of pancakes
+int randomiseProblem()
+{
+    srand(time(NULL));
+    return rand() % 6 + 4;  // in range 3 to 9
+}
+
+int estimateQuality(int result)
+{
+    int qualityMark;
+
+
+
+
+    return qualityMark;
+}
+
+
+int main() 
+{
+    
     return 0;
 }
