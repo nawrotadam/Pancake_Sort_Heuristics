c1912cb069f2c7988022e3ff793b7ab4b817f780
Adam <nawrotadam02@gmail.com>
2020-11-12 15:49:54 +0100

    Added climbing alghoritm implementation, repaired brute force

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import sys
 
 PANCAKES_MIN = 3
-PANCAKES_MAX = 4
+PANCAKES_MAX = 50
 
 
 def randomiseProblem():
@@ -14,6 +14,11 @@ def randomiseProblem():
         randomSize = random.randrange(1, 20)
         if not pancakesArr.__contains__(randomSize):
             pancakesArr.append(randomSize)
+        else:
+            if i == 0:
+                i = 0
+            else:
+                i = i - 1
 
     return pancakesArr
 
@@ -32,11 +37,6 @@ def generateRandomSolution(pancakes):
     swapPosition = random.randrange(0, len(pancakes))  # randomise place where you start to reverse
     pancakes[swapPosition:len(pancakes)] = pancakes[swapPosition:len(pancakes)][::-1]  # reverses part of list in place
     return pancakes
-
-
-# na razie puste
-def nextSolution(solution):
-    print()
 
 
 def printSolution(solution):
@@ -72,35 +72,12 @@ def writeFile(filename, inputData, solution, workTime):
     file.close()
 
 
-# def temp():
-#     # find first element bigger than previous and save its index
-#     splitIndex = 0
-#     biggest =
-#     # todo zmienna zapisujaca najwieksza liczbe i jej index
-#     for i in range(1, len(pancakes)):
-#         if pancakes[i] > pancakes[i - 1]:
-#             splitIndex = i - 1
-#             break  # todo ten break wyleci
-#
-#     # print("splitIndex: " + str(splitIndex))
-#     unsorted = pancakes[splitIndex:]  # unsorted table
-#     indexMax = np.argmax(unsorted) + len(pancakes[0:splitIndex])  # index of biggest pancake
-#
-#     if estimateQuality(pancakes[:splitIndex] + unsorted) == 0:
-#         return pancakes[:splitIndex] + unsorted
-#
-#     # move biggest pancake to the right of the sorted pancakes
-#     np.flip(unsorted[indexMax:])
-#     np.flip(unsorted[splitIndex:])
-#
-#     pancakes = pancakes[:splitIndex] + unsorted  # concatenate final pancakes table
-
 def bruteForce(pancakes):
     unsorted = pancakes.copy()
     sortedNumber = 0
     quality = estimateQuality(pancakes)
     while quality != 0:
-        if sortedNumber == len(pancakes):  # TODO przy gotowym algorytmie to bedzie do wywalenia
+        if sortedNumber == len(pancakes):
             pancakes.reverse()
             return pancakes
         else:
@@ -109,10 +86,12 @@ def bruteForce(pancakes):
         if indexMax == 0:
             unsorted.reverse()
         else:
-            unsorted[:indexMax].append(unsorted[indexMax:].reverse())
+            reversePart = unsorted[indexMax:]
+            unsorted = unsorted[:indexMax]
+            reversePart.reverse()
+            unsorted.extend(reversePart) #to miejsce nie działa
 
         unsorted.reverse()
-
         if sortedNumber == 0:
             pancakes = unsorted.copy()
         else:
@@ -123,26 +102,36 @@ def bruteForce(pancakes):
 
         sortedNumber += 1
         quality = estimateQuality(pancakes)
-
     pancakes.reverse()
     return pancakes
 
 
+def climbingAlghoritm(pancakes):
+    best_pancakes = pancakes
+    best_quality = estimateQuality(best_pancakes)
+    while best_quality != 0:
+        print(best_pancakes, end="")
+        print(" " + str(best_quality))
+        new_pancakes = generateRandomSolution(best_pancakes)
+        new_quality = estimateQuality(new_pancakes)
+        if new_quality < best_quality:
+            best_quality = new_quality
+            best_pancakes = new_pancakes
+    return best_pancakes
+
+
 def main():
-    # startingPancakes = randomiseProblem()  # copy of clean input data to save them to file at the end
-    # pancakes = list.copy(startingPancakes)
+    pancakes = randomiseProblem()
+    startingPancakes = pancakes.copy()
     fileOutput = "output.txt"
     if len(sys.argv) >= 2:  # if aditional terminal parameters are added, override default input/output
         pancakes = sys.argv[1]
         fileOutput = sys.argv[2]
 
-    # TODO DEBUG
-    startingPancakes = [5,3,2,4]
-    pancakes = [5,3,2,4]
-
     startTime = time.time()
     # pancakes = generateRandomSolution(pancakes)
     pancakes = bruteForce(pancakes)
+    # pancakes = climbingAlghoritm(pancakes)
 
     workTime = round(time.time() - startTime, 6)  # work time of alghoritm, rounded to 6 decimal places
     printSolution(pancakes)
